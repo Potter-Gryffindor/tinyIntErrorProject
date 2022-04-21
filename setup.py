969f31ffa78251a18b73bb7187e9a2b77aff83bb
Potter-Gryffindor <jabayter@uninorte.edu.co>
2022-04-20 22:40:04 -0500

    last version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="tinyIntError",
     packages = ['package'],
     version = "0.1",
-    description = "Paquete que permite vañodar errores de tipo tinyInt",
+    description_file = "Paquete que permite vañodar errores de tipo tinyInt",
     author = "Javier Bayter",
     author_email = "jabayter@uninorte.edu.co",
     url = "https://github.com/Potter-Gryffindor/tinyIntErrorProject",
